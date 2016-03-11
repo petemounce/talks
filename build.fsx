00d4b4b7f788c964d411b0f671ae3a8ffff92714
Peter Mounce <peter.mounce@just-eat.com>
2016-03-11 15:13:29 +0000

    tell scripts who i am

--- build.fsx
+++ build.fsx
@@ -9,10 +9,10 @@
 
 // Git configuration (used for publishing documentation in gh-pages branch)
 // The profile where the project is posted
-let gitOwner = "myGitUser"
+let gitOwner = "petemounce"
 let gitHome = "https://github.com/" + gitOwner
 // The name of the project on GitHub
-let gitProjectName = "MyProject"
+let gitProjectName = "talks"
 
 open FsReveal
 open Fake
@@ -37,11 +37,11 @@ Target "Clean" (fun _ ->
     CleanDirs [outDir]
 )
 
-let fsiEvaluator = 
+let fsiEvaluator =
     let evaluator = FSharp.Literate.FsiEvaluator()
-    evaluator.EvaluationFailed.Add(fun err -> 
+    evaluator.EvaluationFailed.Add(fun err ->
         traceImportant <| sprintf "Evaluating F# snippet failed:\n%s\nThe snippet evaluated:\n%s" err.StdErr err.Text )
-    evaluator 
+    evaluator
 
 let copyStylesheet() =
     try
@@ -55,15 +55,15 @@ let copyPics() =
     with
     | exn -> traceImportant <| sprintf "Could not copy picture: %s" exn.Message
 
-let generateFor (file:FileInfo) = 
+let generateFor (file:FileInfo) =
     try
         copyPics()
         let rec tryGenerate trials =
             try
                 FsReveal.GenerateFromFile(file.FullName, outDir, fsiEvaluator = fsiEvaluator)
-            with 
+            with
             | exn when trials > 0 -> tryGenerate (trials - 1)
-            | exn -> 
+            | exn ->
                 traceImportant <| sprintf "Could not generate slides for: %s" file.FullName
                 traceImportant exn.Message
 
@@ -90,7 +90,7 @@ let socketHandler (webSocket : WebSocket) =
     while true do
       let! refreshed =
         Control.Async.AwaitEvent(refreshEvent.Publish)
-        |> Suave.Sockets.SocketOp.ofAsync 
+        |> Suave.Sockets.SocketOp.ofAsync
       do! webSocket.send Text (ASCII.bytes "refreshed") true
   }
 
@@ -105,7 +105,7 @@ let startWebServer () =
 
     let port = findPort 8083
 
-    let serverConfig = 
+    let serverConfig =
         { defaultConfig with
            homeFolder = Some (FullName outDir)
            bindings = [ HttpBinding.mkSimple HTTP "127.0.0.1" port ]
@@ -129,7 +129,7 @@ Target "GenerateSlides" (fun _ ->
 
 Target "KeepRunning" (fun _ ->
     use watcher = !! (slidesDir + "/**/*.*") |> WatchChanges handleWatcherEvents
-    
+
     startWebServer ()
 
     traceImportant "Waiting for slide edits. Press any key to stop."
@@ -159,5 +159,5 @@ Target "ReleaseSlides" (fun _ ->
 
 "GenerateSlides"
   ==> "ReleaseSlides"
-  
+
 RunTargetOrDefault "KeepRunning"
